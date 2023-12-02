ab8098198edd7942b8a0093c2f28a9a565b36c31
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-02 23:30:24 +0700

    remove unused struct

--- main.go
+++ main.go
@@ -14,11 +14,6 @@ import (
 	"time"
 )
 
-type login struct {
-	Username string `form:"username" json:"username" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
-}
-
 func helloHandler(c *gin.Context) {
 	claims := jwt.ExtractClaims(c)
 	user, _ := c.Get(identityKey)
