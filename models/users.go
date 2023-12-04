8bb82e2ae71c13e83f6b62788c607c34abd23926
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-04 07:22:47 +0700

    add other properties in user: password, pin, status, phone_verification_status, and email_verification_status

--- models/users.go
+++ models/users.go
@@ -1,8 +1,29 @@
 package models
 
+const (
+	StatusVerified = 1
+)
+
 type Users struct {
-	Id          uint   `json:"id"`
-	Email       string `json:"email"`
-	PhoneNumber string `json:"phone_number"`
-	Name        string `json:"name"`
+	Id                      uint   `json:"id"`
+	Email                   string `json:"email"`
+	PhoneNumber             string `json:"phone_number"`
+	Name                    string `json:"name"`
+	Password                string `json:"password"`
+	Pin                     string `json:"pin"`
+	Status                  int    `json:"status"`
+	PhoneVerificationStatus int    `json:"phone_verification_status"`
+	EmailVerificationStatus int    `json:"email_verification_status"`
+}
+
+func (users Users) IsEmailVerified() bool {
+	return users.EmailVerificationStatus == StatusVerified
+}
+
+func (users Users) IsPhoneVerified() bool {
+	return users.PhoneVerificationStatus == StatusVerified
+}
+
+func (users Users) IsPinRegistered() bool {
+	return users.Pin != ""
 }
