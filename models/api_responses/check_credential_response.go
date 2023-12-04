6c729f1a2059346c913a5db866fd7ce15ef6a09e
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-04 07:22:55 +0700

    create struct CheckCredentialResponse

---
+++ models/api_responses/check_credential_response.go
@@ -0,0 +1,11 @@
+package api_responses
+
+type CheckCredentialResponse struct {
+	Email           string `json:"email"`
+	IsEmailVerified bool   `json:"is_email_verified"`
+	IsPhoneVerified bool   `json:"is_phone_verified"`
+	IsPinRegistered bool   `json:"is_pin_registered"`
+	IsRegistered    bool   `json:"is_registered"`
+	IsUserExist     bool   `json:"is_user_exist"`
+	PhoneNumber     string `json:"phone_number"`
+}
