35e264faced50e4d2f5bf077008d7aef1854f450
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-05 07:54:33 +0700

    feature: add update phone number to controller

--- controllers/api/v1/user_controller/update_phone_number.go
+++ controllers/api/v1/user_controller/update_phone_number.go
@@ -5,7 +5,6 @@ import (
 	"flop/middleware"
 	"flop/models/database_model"
 	"flop/repositories/users_repository"
-	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,7 +20,7 @@ import (
 //	@Param			api_key	header string	true "Api Key"
 //	@Param			phone_number formData string	true "Phone number that will be saved to the database_model"
 //	@Security		ApiKeyAuth
-func UpdatePhoneNumber(c *gin.Context, authMiddleware *jwt.GinJWTMiddleware) {
+func UpdatePhoneNumber(c *gin.Context) {
 	currentUserId := middleware.GetUserIdFromJWT(c)
 	phoneNumber := c.Request.FormValue("phone_number")
 
