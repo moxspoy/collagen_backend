d17c9aa28512c137152cf1f062db605fb456237b
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-08 22:18:24 +0700

    create function SetupDatabase

--- config/database/setup.go
+++ config/database/setup.go
@@ -3,17 +3,31 @@ package database
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
 )
 
 var DB *gorm.DB
 
-func ConnectDatabase() {
+func ConnectDatabase() error {
 	dsn := "root:rootroot@tcp(127.0.0.1:3306)/flop?charset=utf8mb4&parseTime=True&loc=Local"
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		panic("Failed to connect to database!")
+		return err
 	}
 
 	DB = database
+	return nil
+}
+
+func SetupDatabase() {
+	err := ConnectDatabase()
+	if err != nil {
+		return
+	}
+
+	err = RunAutoMigration(DB)
+	if err != nil {
+		log.Fatal("Migration Error:" + err.Error())
+	}
 }
