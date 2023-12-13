aaa9a848abb03c532f5205c332ade273386a16b2
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-13 08:45:34 +0700

    required otp for existing user in update phone number

--- controllers/api/v1/user_controller/update_phone_number.go
+++ controllers/api/v1/user_controller/update_phone_number.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flop/helper/api_response_helper"
 	"flop/middleware"
+	"flop/repositories/one_time_password_repository"
 	"flop/repositories/user_repository"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -12,7 +13,7 @@ import (
 // UpdatePhoneNumber godoc
 //
 //	@Summary		Update user's phone number
-//	@Description	Usually this endpoint used as part of onboarding. Note that it should contains country code like +6285911110000
+//	@Description	Usually this endpoint used as part of onboarding. Note that it should contain country code like +6285911110000. Note that user need to request otp first.
 //	@Tags			User
 //	@Accept			multipart/form-data
 //	@Produce		json
@@ -20,10 +21,12 @@ import (
 //	@Router			/user/update-phone-number [put]
 //	@Param			api_key	header string	true "Api Key"
 //	@Param			phone_number formData string	true "Phone number that will be saved to the database_model"
+//	@Param			otp formData string	false "OTP for authentication (if pin already exist)"
 //	@Security		ApiKeyAuth
 func UpdatePhoneNumber(c *gin.Context) {
 	currentUserId := middleware.GetUserIdFromJWT(c)
 	phoneNumber := c.Request.FormValue("phone_number")
+	otp := c.Request.FormValue("otp")
 
 	if phoneNumber == "" {
 		api_response_helper.GenerateErrorResponse(c, errors.New("phone number can not be empty"))
@@ -31,15 +34,32 @@ func UpdatePhoneNumber(c *gin.Context) {
 	}
 
 	validate := validator.New(validator.WithRequiredStructEnabled())
-	errs := validate.Var(phoneNumber, "required,e164")
 
+	errs := validate.Var(phoneNumber, "required,e164")
 	if errs != nil {
 		api_response_helper.GenerateErrorResponse(c, errs)
 		return
 	}
 
-	if dbc := user_repository.UpdatePhoneNumber(currentUserId, phoneNumber); dbc.Error != nil {
-		api_response_helper.GenerateErrorResponse(c, dbc.Error)
+	// Check OTP
+	user := user_repository.GetOneUserById(currentUserId)
+	if user.IsPhoneVerified() {
+		errs = validate.Var(otp, "required,numeric")
+		if errs != nil {
+			api_response_helper.GenerateErrorResponse(c, errs)
+			return
+		}
+
+		errs = one_time_password_repository.CheckOneTimePassword(currentUserId, otp)
+
+		if errs != nil {
+			api_response_helper.GenerateErrorResponse(c, errs)
+			return
+		}
+	}
+
+	if tx := user_repository.UpdatePhoneNumber(currentUserId, phoneNumber); tx.Error != nil {
+		api_response_helper.GenerateErrorResponse(c, tx.Error)
 		return
 	}
 
