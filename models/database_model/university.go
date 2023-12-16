9b3e4a11dd1752f5f193cff1db582dc9372f6f11
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-16 09:30:41 +0700

    create university data model

---
+++ models/database_model/university.go
@@ -0,0 +1,26 @@
+package database_model
+
+import (
+	"gorm.io/gorm"
+)
+
+type University struct {
+	gorm.Model
+	Name    string
+	NameEn  string
+	Acronym string
+
+	// Active (1) or Not Active (2)
+	Status int16
+
+	// State owned (1) or private (2)
+	Category int16
+
+	Website     string
+	YearFounded int16
+	AddressID   int16
+}
+
+func (University) TableName() string {
+	return "university"
+}
