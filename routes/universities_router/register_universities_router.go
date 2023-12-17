f223437b95e557c898836be419b95473b3fa8cf3
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-17 13:28:06 +0700

    setup router of university table

---
+++ routes/universities_router/register_universities_router.go
@@ -0,0 +1,15 @@
+package universities_router
+
+import (
+	"collagen/controllers/api/v1/universities_controller"
+	jwt "github.com/appleboy/gin-jwt/v2"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUniversitiesRouter(v1Router *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) {
+	universitiesRouter := v1Router.Group("/universities")
+	universitiesRouter.Use(authMiddleware.MiddlewareFunc())
+	{
+		universitiesRouter.GET("/all", universities_controller.GetAllUniversities)
+	}
+}
