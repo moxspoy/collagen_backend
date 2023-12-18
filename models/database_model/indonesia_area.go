6863d61d129531e2b551509a16568588ea330c96
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-18 08:21:54 +0700

    add github docs for sql dump

--- models/database_model/indonesia_area.go
+++ models/database_model/indonesia_area.go
@@ -4,6 +4,8 @@ import (
 	"gorm.io/gorm"
 )
 
+// database from https://github.com/cahyadsn/wilayah/blob/master/db/wilayah.sql
+
 type IndonesiaArea struct {
 	gorm.Model
 	Code string `binding:"required"`
