168671add695cdca3bdcecdcf6d8892de2bb8dce
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-23 06:20:39 +0700

    add num likes, num of dislikes, and num reports

--- models/database_model/comment.go
+++ models/database_model/comment.go
@@ -4,9 +4,12 @@ import "gorm.io/gorm"
 
 type Comment struct {
 	gorm.Model
-	Content string `gorm:"not null"`
-	UserID  uint   `gorm:"not null"`
-	User    User   `gorm:"foreignKey:UserID"`
-	PostID  uint   `gorm:"not null"`
-	Post    Post   `gorm:"foreignKey:PostID"`
+	Content     string `gorm:"not null"`
+	UserID      uint   `gorm:"not null"`
+	User        User   `gorm:"foreignKey:UserID"`
+	PostID      uint   `gorm:"not null"`
+	Post        Post   `gorm:"foreignKey:PostID"`
+	NumLikes    uint   `gorm:"default:0"`
+	NumDislikes uint   `gorm:"default:0"`
+	NumReports  uint   `gorm:"default:0"`
 }
