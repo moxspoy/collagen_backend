a62169eb383ece90307c3d57d9057e61d65b6078
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-23 06:31:08 +0700

    remove lower case json in user detail

--- models/database_model/user_logged_in_devices.go
+++ models/database_model/user_logged_in_devices.go
@@ -6,15 +6,15 @@ import (
 
 type UserLoggedInDevices struct {
 	gorm.Model
-	UserID           uint   `json:"user_id"`
-	User             User   `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	DeviceModel      string `json:"device_model"`
-	DeviceIdentifier string `json:"device_identifier"`
-	OsVersion        string `json:"os_version"`
-	LastLogin        string `json:"last_login"`
-	Platform         string `json:"platform"`
-	AppNameVersion   string `json:"app_name_version"`
-	AppBuildVersion  int    `json:"app_build_version"`
+	UserID           uint
+	User             User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	DeviceModel      string
+	DeviceIdentifier string
+	OsVersion        string
+	LastLogin        string
+	Platform         string
+	AppNameVersion   string
+	AppBuildVersion  int
 }
 
 func (UserLoggedInDevices) TableName() string {
