c561f555c699f9eb010e5caf58a67bf2bef5f9d2
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-24 08:06:14 +0700

    setup router post

---
+++ routes/post_router/register_post_router.go
@@ -0,0 +1,16 @@
+package post_router
+
+import (
+	"collagen/controllers/api/v1/post_controller"
+	jwt "github.com/appleboy/gin-jwt/v2"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterPostRouter(v1Router *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) {
+	postRouter := v1Router.Group("/post")
+	postRouter.Use(authMiddleware.MiddlewareFunc())
+	{
+		postRouter.GET("/", post_controller.GetPosts)
+		postRouter.POST("/", post_controller.CreatePost)
+	}
+}
