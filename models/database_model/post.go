bc0386dd1393466238ae590dd6595a61118f2c59
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-12-24 08:05:21 +0700

    remove explicit CreatedAt and UpdatedAt

--- models/database_model/post.go
+++ models/database_model/post.go
@@ -2,21 +2,17 @@ package database_model
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type Post struct {
 	gorm.Model
 	Title       string `gorm:"not null"`
 	Content     string `gorm:"not null"`
+	UserID      uint   `gorm:"not null"`
+	User        User   `gorm:"foreignKey:UserID"`
+	NumLikes    uint   `gorm:"default:0"`
+	NumDislikes uint   `gorm:"default:0"`
+	NumReports  uint   `gorm:"default:0"`
+	NumShares   uint   `gorm:"default:0"`
 	ImageURL    string
-	UserID      uint      `gorm:"not null"`
-	User        User      `gorm:"foreignKey:UserID"`
-	CreatedAt   time.Time `gorm:"not null"`
-	UpdatedAt   time.Time `gorm:"not null"`
-	NumLikes    uint      `gorm:"default:0"`
-	NumDislikes uint      `gorm:"default:0"`
-	NumReports  uint      `gorm:"default:0"`
-	NumShares   uint      `gorm:"default:0"`
-	Comments    []Comment
 }
