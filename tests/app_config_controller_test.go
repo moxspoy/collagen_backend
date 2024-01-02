3ff533db9073e06a5662961047001564ae4085ea
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2024-01-02 23:06:25 +0700

    use test helper in controller test

--- tests/app_config_controller_test.go
+++ tests/app_config_controller_test.go
@@ -1,50 +1,19 @@
 package tests
 
 import (
-	"collagen/config/database"
 	"collagen/models/database_model"
-	"collagen/routes/central_router"
+	"collagen/tests/test_helper"
 	"encoding/json"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
-	"log"
 	"net/http"
-	"net/http/httptest"
-	"os"
 	"testing"
 )
 
 func TestAppConfigController(t *testing.T) {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	requiredApiKey := os.Getenv("API_KEY")
-
-	// Initialize a test router
-	router := gin.Default()
-
-	// Set the test mode to disable unnecessary output
-	gin.SetMode(gin.TestMode)
-	central_router.RegisterAllRouter(router)
-
-	// Create a temporary in-memory database for testing
-	database.DB, _ = database.SetupTestDB()
-	err = database.RunAutoMigration(database.DB)
-	if err != nil {
-		return
-	}
+	router := test_helper.SetupServerBeforeTest()
 
 	t.Run("Get App Info", func(t *testing.T) {
-
-		// Perform a POST request to create a new post
-		req, _ := http.NewRequest("GET", "/api/v1/app-info", nil)
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("api_key", requiredApiKey)
-
-		w := httptest.NewRecorder()
-		router.ServeHTTP(w, req)
+		w := test_helper.RequestApiForTest(router, "GET", "/api/v1/app-info", nil)
 
 		// Check if the status code is OK
 		assert.Equal(t, http.StatusOK, w.Code)
@@ -56,7 +25,6 @@ func TestAppConfigController(t *testing.T) {
 			return
 		}
 		assert.Equal(t, "", "")
-
 	})
 
 }
