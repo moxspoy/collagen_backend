412525816afd454211b8e388cb6d4535a77e1b29
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2024-01-03 21:50:38 +0700

    user public api

--- tests/app_config_controller_test.go
+++ tests/app_config_controller_test.go
@@ -13,7 +13,7 @@ func TestAppConfigController(t *testing.T) {
 	router := test_helper.SetupServerBeforeTest()
 
 	t.Run("Get App Info", func(t *testing.T) {
-		w := test_helper.RequestApiForTest(router, "GET", "/api/v1/app-info", nil)
+		w := test_helper.RequestPublicApiForTest(router, "GET", "/api/v1/app-info", nil)
 
 		// Check if the status code is OK
 		assert.Equal(t, http.StatusOK, w.Code)
