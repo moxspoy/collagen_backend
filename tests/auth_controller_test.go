6c1158a808ae5bf8e0e49f0fe0560568bce9468d
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2024-01-08 19:04:23 +0700

    save the token after sign up in test

--- tests/auth_controller_test.go
+++ tests/auth_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"collagen/models/api_response_model"
 	"collagen/tests/test_helper"
 	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"strings"
@@ -13,6 +14,7 @@ import (
 
 func TestAuthController(t *testing.T) {
 	router := test_helper.SetupServerBeforeTest()
+	var token string
 
 	t.Run("Sign Up", func(t *testing.T) {
 		request := api_request_model.UserSignUpRequest{
@@ -33,12 +35,15 @@ func TestAuthController(t *testing.T) {
 
 		assert.Equal(t, http.StatusOK, w.Code)
 
-		var response api_response_model.SuccessAPIResponse
+		var response api_response_model.SuccessAPIResponseWithToken
 		err := json.Unmarshal(w.Body.Bytes(), &response)
 		if err != nil {
 			return
 		}
 
+		token = response.Data.Token
 		assert.Equal(t, response.Message, "success register")
+		fmt.Print("token", token)
 	})
+
 }
