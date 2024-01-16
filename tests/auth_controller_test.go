7c60d3df8eeb9fcabd8131dcb836e453e2388b1f
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2024-01-16 23:19:35 +0700

    test: api check credential

--- tests/auth_controller_test.go
+++ tests/auth_controller_test.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"net/url"
 	"strings"
 	"testing"
 )
@@ -16,13 +17,44 @@ func TestAuthController(t *testing.T) {
 	router := test_helper.SetupServerBeforeTest()
 	var token string
 
-	t.Run("Check Credential", func(t *testing.T) {
+	t.Run("Check Wrong Credential", func(t *testing.T) {
 		request := ""
 		requestJSON, _ := json.Marshal(request)
 
-		w := test_helper.RequestApiWithoutTokenForTest(router, "POST", "/api/v1/auth/check-credential", strings.NewReader(string(requestJSON)))
+		w := test_helper.RequestApiWithFormDataWithoutTokenForTest(router, "POST", "/api/v1/auth/check-credential", strings.NewReader(string(requestJSON)))
 
 		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+
+	t.Run("Check Correct Credential With User Not In Database", func(t *testing.T) {
+
+		// Prepare a sample form data
+		formData := url.Values{
+			"credential": {"eqi@gmail.com"},
+		}
+
+		w := test_helper.RequestApiWithFormDataWithoutTokenForTest(router, "POST", "/api/v1/auth/check-credential", strings.NewReader(formData.Encode()))
+
+		// Print information about the response (optional)
+		fmt.Println("Response Code:", w.Code)
+		fmt.Println("Response Body:", w.Body.String())
+
+		// Assertions and validations for the response
+		assert.Equal(t, http.StatusOK, w.Code)
+
+		// Parse the response body
+		var responseBody api_response_model.CheckCredentialResponse
+		err := json.Unmarshal(w.Body.Bytes(), &responseBody)
+		assert.NoError(t, err, "Error parsing response body")
+
+		// Example assertions based on the CheckCredentialResponse struct
+		assert.Equal(t, "", responseBody.Email)
+		assert.False(t, responseBody.IsEmailVerified)
+		assert.False(t, responseBody.IsPhoneVerified)
+		assert.False(t, responseBody.IsPinRegistered)
+		assert.False(t, responseBody.IsRegistered)
+		assert.False(t, responseBody.IsUserExist)
+		assert.Equal(t, "", responseBody.PhoneNumber)
 	})
 
 	t.Run("Sign Up", func(t *testing.T) {
